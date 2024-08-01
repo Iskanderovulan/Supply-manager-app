bc2a05631035ef29a2bc53619370dc3164fa11b5
iskanderov ulan <iskanderovulan@gmail.com>
2024-08-01 22:01:22 +0600

    Applink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "app/providers/router";
+
 const App = () => {
     return (
         <div className={classNames("app", {}, [])}>
