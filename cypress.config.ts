c7f053f1d4d91f49eab36f66f697903efb46779a
iskanderov ulan <iskanderovulan@gmail.com>
2025-02-06 03:20:41 +0600

    e2e ci/cd attemp5

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
     e2e: {
-        baseUrl: process.env.CI ? "http://localhost:5000/" : "http://localhost:5173/", 
+        baseUrl: process.env.CI ? "http://localhost:4173/" : "http://localhost:5173/",
         supportFile: "cypress/support/e2e.ts",
         specPattern: "cypress/e2e/**/*.cy.{js,ts}",
     },
