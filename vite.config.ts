0ebf13ae67c2f72b2992deb6b111ab0b4a8475fe
iskanderov ulan <iskanderovulan@gmail.com>
2025-06-24 18:45:53 +0600

    aliases viteconfig fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,33 +4,36 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 import svgr from "@svgr/rollup";
 
-export default defineConfig(({ mode }) => {
-    return {
-        plugins: [react(), tsconfigPaths(), svgr()],
+const aliases = {
+    "@app": path.resolve(__dirname, "./src/app"),
+    "@pages": path.resolve(__dirname, "./src/pages"),
+    "@widgets": path.resolve(__dirname, "./src/widgets"),
+    "@features": path.resolve(__dirname, "./src/features"),
+    "@entities": path.resolve(__dirname, "./src/entities"),
+    "@shared": path.resolve(__dirname, "./src/shared"),
+};
+
+export default defineConfig(({ mode }) => ({
+    plugins: [react(), tsconfigPaths(), svgr()],
+    resolve: {
+        alias: aliases,
+    },
+    define: {
+        __IS_DEV__: mode === "development",
+    },
+    optimizeDeps: {
+        exclude: ["recharts"],
+    },
+    server: {
+        host: true,
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        css: true,
         resolve: {
-            alias: {
-                "@app": path.resolve(__dirname, "./src/app"),
-                "@pages": path.resolve(__dirname, "./src/pages"),
-                "@widgets": path.resolve(__dirname, "./src/widgets"),
-                "@features": path.resolve(__dirname, "./src/features"),
-                "@entities": path.resolve(__dirname, "./src/entities"),
-                "@shared": path.resolve(__dirname, "./src/shared"),
-            },
+            alias: aliases,
         },
-        define: {
-            __IS_DEV__: mode === "development",
-        },
-        optimizeDeps: {
-            exclude: ["recharts"],
-        },
-        test: {
-            globals: true,
-            environment: "jsdom",
-            setupFiles: "./src/setupTests.ts",
-            css: true,
-        },
-        server: {
-            host: true, // или '0.0.0.0'
-        },
-    };
-});
+    },
+}));
